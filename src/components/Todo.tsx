4b474253ec5decbcfcc6c1d94b12f63c17189205
kabasunny <dxbqd553@yahoo.co.jp>
2024-08-07 16:32:17 +0900

    auth

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,22 @@
 
+import {
+  ArrowRightOnRectangleIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/24/solid'
+import useStore from '../store'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
 export const Todo = () => {
+  const { logoutMutation } = useMutateAuth()
+  const logout = async () => {
+    await logoutMutation.mutateAsync()
+  }
   return (
-    <div>Todo</div>
+    <div>
+      <ArrowRightOnRectangleIcon
+        onClick={logout}
+        className="h-6 w-6 my-6 text-blue-500 cursor-pointer"
+      />
+    </div>
   )
 }
